2a4426dc65ac4af42b5080116d522b9454d1dabb
Earn <earn@keptcarbon.local>
2026-05-29 17:05:36 +0700

    ีup

--- nextjs/app/components/organisms/Header/Header.tsx
+++ nextjs/app/components/organisms/Header/Header.tsx
@@ -339,14 +339,6 @@ export default function Header() {
             </div>
           </div>
 
-          <div className="drawer-footer">
-            <i className="bi bi-leaf-fill"></i>
-            <div className="footer-title" style={{ fontSize: '13px', lineHeight: '1.5', color: '#6a7c70', textAlign: 'center' }}>
-              แพลตฟอร์มภูมิสารสนเทศและปัญญาประดิษฐ์<br />
-              เพื่อการจัดการสวนยางพารา
-            </div>
-
-          </div>
         </div>
       </div>
     </>
